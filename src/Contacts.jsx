8b19874739e20449da2430f381607df6d11a3ab6
Pvag <paolondon@gmail.com>
2024-05-26 11:42:58 +0700

    basic routing test

---
+++ src/Contacts.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import NavBar from "./NavBar"
+
+function Contacts() {
+  return (
+    <>
+      <NavBar />
+      <h1>Contacts</h1>
+    </>
+  )
+}
+
+export default Contacts
